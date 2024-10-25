078656c3642b8e13ac80e746bab9974e3af36c6f
robust785 <167668570+robust785@users.noreply.github.com>
2024-10-25 08:40:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ app.use(express.json());
 app.use(auth);
 app.use(product);
 
+app.get("/about",(req,res)=>{
+    res.status(200).send("Made by MAYUR KUNDE");
+});
 
 //DB connectivity
 const PORT=process.env.PORT ||3000;
